fb4244ff0f23bcb35d4b2005a53ce6313a1a85a9
nikola@asial.co.jp <nikola@asial.co.jp>
2017-02-02 02:09:34 +0000

    update file /application.js

--- application.js
+++ application.js
@@ -23,7 +23,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(mbaasExpress.fhmiddleware());
 
 app.use('/hello', require('./lib/hello.js')());
-app.use('/hello', require('./lib/users.js')());
+app.use('/users', require('./lib/users.js')());
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
